dee561673bb3e85beb655c1fe0484ddfe12adb4a
Arjan Malda <95175291+arjanmalda@users.noreply.github.com>
2022-12-06 07:49:17 +0100

    Prettify

--- index.ts
+++ index.ts
@@ -1,31 +1,26 @@
 const fs = require('fs');
 
+fs.readFile('puzzle-input.txt', 'utf8', function(error:  unknown, data: string) {
 
+    if (error) return error;
 
-    fs.readFile('puzzle-input.txt', 'utf8', function(err, data) {
-    // if (err) throw err;
     const dataWithoutLineBreaks = data.split(/\n/);
-
     const arrayOfSums = [];
     let temporaryArray = [];
+
     for (let index = 0; index < dataWithoutLineBreaks.length; index++) {
         
-    if (dataWithoutLineBreaks[index].length > 1){
-    temporaryArray.push(parseInt(dataWithoutLineBreaks[index]))};
+        if (dataWithoutLineBreaks[index].length > 1){
+        temporaryArray.push(parseInt(dataWithoutLineBreaks[index]))};
 
-    if (dataWithoutLineBreaks[index].length < 1){
+        if (dataWithoutLineBreaks[index].length < 1){
         const temporaryArraySum = temporaryArray.reduce((partialSum, a) => partialSum + a, 0);
         arrayOfSums.push(temporaryArraySum);
         temporaryArray = [];
+      }
     }
-
-
-
-
-    }
-    console.log(Math.max(...arrayOfSums));
-
-});
+  }
+);
 
 
 
